0fb40799bb69ed7e534ceaef112ea693c0d2a302
Michael Karneim <mkarneim@googlemail.com>
2017-04-30 09:00:43 +0200

    Move Apple to its own file

--- src/main/java/fruits/Apple.java
+++ src/main/java/fruits/Apple.java
@@ -4,7 +4,7 @@ import net.karneim.pojobuilder.GenerateBuilderInterface;
 import net.karneim.pojobuilder.GeneratePojoBuilder;
 
 @GenerateBuilderInterface(withName = "I*Builder")
-@GeneratePojoBuilder(withBuilderInterface = IFruitBuilder.class)
-public class Fruit {
-  public String colour;
+@GeneratePojoBuilder(withBuilderInterface = IAppleBuilder.class)
+public class Apple extends Fruit {
+  public String variety;
 }
